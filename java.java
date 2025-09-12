c1ded24156dee99d69e8df0a69468cdd7226340c
kiwakuku <hamsterkiller06@mail.ru>
2025-09-12 17:33:21 +0300

    Add files via upload

---
+++ java.java
@@ -0,0 +1,25 @@
+#Создание списка
+
+public class ListExample {
+    public static void main(String[] args) {
+        List myList = new ArrayList();
+        myList.add("Кружка");
+        myList.add("Стакан");
+        myList.add("Вилка");
+    }
+}
+
+
+#Создание стека
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+
+
+        stack.push(100);
+        stack.push(110);
+        stack.push(120);
+    }
+}
