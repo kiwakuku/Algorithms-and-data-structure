9ad887b2c258dc13ddec7a8d32188d24caed8385
kiwakuku <hamsterkiller06@mail.ru>
2025-09-28 13:33:32 +0300

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,11 +1,12 @@
 hash = {}
 
-hash["яблоко"] = 150  
-hash["банан"] = 200   
-hash["апельсин"] = 180 
+hash["apple"] = 150  
+hash["banana"] = 200   
+hash["orange"] = 180 
 
 for key, value in hash.items():
     print(f"{key},{value}")
+
 
 
 
