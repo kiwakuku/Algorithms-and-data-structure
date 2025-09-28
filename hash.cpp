69b685aa0de5abe0bbfcb1401e9b438a1420f53f
kiwakuku <hamsterkiller06@mail.ru>
2025-09-28 13:23:09 +0300

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -5,7 +5,8 @@ int main() {
     std::unordered_map<std::string, int> ht = {{"apple", 10}, {"banana", 20}, {"cherry", 30}};
 
     for (const auto& p : ht)
-        std::cout << p.first << " -> " << p.second << "\n";
+        std::cout << p.first << " " << p.second << "\n";
 
     return 0;
 }
+
