7099c7bc197acf98f9d6024e15f285fa5ece919a
kiwakuku <hamsterkiller06@mail.ru>
2025-09-28 13:21:45 +0300

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,10 +1,12 @@
 import heapq
 heap = []
 heapq.heappush(heap, 3)
-heapq.heappush(heap, 1)
+heapq.heappush(heap, 2)
 heapq.heappush(heap, 4)
 heapq.heappush(heap, 1)
 heapq.heappush(heap, 5)
 
-print(f"Куча после добавления: {heap}") 
+print(f"{heap}") 
+
+
 
