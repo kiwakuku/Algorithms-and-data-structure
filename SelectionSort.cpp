2618949e945297df0a85e1dacb5fe8605fa7d505
kiwakuku <hamsterkiller06@mail.ru>
2025-10-26 11:08:23 +0300

    Update SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,44 +1,34 @@
 #include <iostream>
 using namespace std;
 
-// Функция для вывода массива
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
-
-// Реализация сортировки выбором
 void selectionSort(int arr[], int n) {
-    // Проходим по каждому элементу массива
-    for (int i = 0; i < n - 1; ++i) {     // i - индекс текущего минимального элемента
-        int minIndex = i;                 // Предполагаемый минимальный элемент
+    for (int i = 0; i < n - 1; ++i) { // Проходим цикл по массиву
+        int minIndex = i;              // Предполагаемый минимальный элемент
+
+        // Поиск минимального элемента среди оставшихся элементов массива
+        for (int j = i + 1; j < n; ++j)
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
         
-        // Ищем минимальное значение справа от текущего индекса
-        for (int j = i + 1; j < n; ++j) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;              // Обновляем индекс минимума
-            }
-        }
-    
-        // Меняем найденный минимальный элемент с текущим
+        // Меняем местами найденный минимум с элементом на текущей позиции
         swap(arr[i], arr[minIndex]);
     }
 }
 
-// Основной метод
+// Тестовая функция
 int main() {
     int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr)/sizeof(arr[0]);      // Вычисление размера массива
+    int n = sizeof(arr)/sizeof(arr[0]);
     
     cout << "Исходный массив:\n";
-    printArray(arr, n);
-    
-    selectionSort(arr, n);                   // Сортируем массив
-    
-    cout << "\nОтсортированный массив:\n";
-    printArray(arr, n);
-    
+    for (int i : arr)
+        cout << i << ' ';
+    cout << '\n';
+
+    selectionSort(arr, n);
+
+    cout << "Отсортированный массив:\n";
+    for (int i : arr)
+        cout << i << ' ';
     return 0;
 }
